a984eab21b2ea1318031e3404d272b36362d3c77
Bas van Hoof <bashooff@hotmail.com>
2021-11-11 22:20:30 +0100

    Update Sidebar.js

--- frontend/src/components/widgets/Sidebar.js
+++ frontend/src/components/widgets/Sidebar.js
@@ -2,56 +2,12 @@ import styles from './Sidebar.module.css'
 
 const Sidebar = () => {
     return(
-        <div class={styles.wrapper}>
-   
-    <nav id="sidebar">
-        <div class="sidebar-header">
-            <h3>Bootstrap Sidebar</h3>
+        <div id="mySidebar" className="sidebar">
+            <a href="#">Home</a>
+            <a href="#">Aout</a>
+            <a href="#">Loans</a>
+            <a href="#">Contact</a>
         </div>
-
-        <ul class="list-unstyled components">
-            <p>Dummy Heading</p>
-            <li class="active">
-                <a href="#homeSubmenu" data-toggle="collapse" aria-expanded="false" class="dropdown-toggle">Home</a>
-                <ul class="collapse list-unstyled" id="homeSubmenu">
-                    <li>
-                        <a href="#">Home 1</a>
-                    </li>
-                    <li>
-                        <a href="#">Home 2</a>
-                    </li>
-                    <li>
-                        <a href="#">Home 3</a>
-                    </li>
-                </ul>
-            </li>
-            <li>
-                <a href="#">About</a>
-            </li>
-            <li>
-                <a href="#pageSubmenu" data-toggle="collapse" aria-expanded="false" class="dropdown-toggle">Pages</a>
-                <ul class="collapse list-unstyled" id="pageSubmenu">
-                    <li>
-                        <a href="#">Page 1</a>
-                    </li>
-                    <li>
-                        <a href="#">Page 2</a>
-                    </li>
-                    <li>
-                        <a href="#">Page 3</a>
-                    </li>
-                </ul>
-            </li>
-            <li>
-                <a href="#">Portfolio</a>
-            </li>
-            <li>
-                <a href="#">Contact</a>
-            </li>
-        </ul>
-    </nav>
-
-</div>
     )
 }
 
