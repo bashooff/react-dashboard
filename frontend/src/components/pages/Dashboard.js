99914a4c170fbd9eea1aea99818caadc571198f5
Bas van Hoof <bas.van.hoof@nl.pwc.com>
2021-11-18 09:14:10 +0100

    card for dashboard

--- frontend/src/components/pages/Dashboard.js
+++ frontend/src/components/pages/Dashboard.js
@@ -13,66 +13,75 @@ const Dashboard = () => {
         <div>
             <h2>Dashboard overview</h2>
             <div className="row">
-                <div className="col-md-3 my-col">
-                    <Card title="Borrowers" text="borrowers overview" height="10rem" width="22rem">
+                <Card title="Overview" height="18rem" width="60rem">
+                <div className="row mt-5">
+                    
+                    <div className="col  my-col">
+                        <Card title="Borrowers" text="borrowers overview" height="10rem" width="16rem">
+                            <div className="card-icon">
+                                <FcPackage></FcPackage>
+                            </div>
+                            <div className="card-title">
+                                Borrowers
+                            </div>
+                            <div className="card-text">
+                                1,200
+                            </div>
+                        </Card>
+                    </div>
+                    <div className="col my-col">
+                        <Card title="Loans" height="10rem" width="16rem">
                         <div className="card-icon">
-                            <FcPackage></FcPackage>
-                        </div>
-                        <div className="card-title">
-                            Borrowers
-                        </div>
-                        <div className="card-text">
-                            1,200
-                        </div>
-                    </Card>
-                </div>
-                <div className="col-md-3 my-col">
-                    <Card title="Loans" height="10rem" width="22rem">
-                    <div className="card-icon">
-                            <FcList></FcList>
-                        </div>
-                        <div className="card-title">
-                            Loans
-                        </div>
-                        <div className="card-text">
-                            1,356
-                        </div>
-                    </Card>
-                </div>
-                <div className="col-md-3 my-col">
-                    <Card title="returns" height="10rem" width="22rem">
-                    <div className="card-icon">
-                            <FcSalesPerformance></FcSalesPerformance>
-                        </div>
-                        <div className="card-title">
-                            Portfolio return
-                        </div>
-                        <div className="card-text">
-                            14,65%
-                        </div>
-                    </Card>
-                </div>
-                <div className="col-md-3 my-col">
-                    <Card title="returns" height="10rem" width="22rem">
-                    <div className="card-icon">
-                            <FcBookmark></FcBookmark>
-                        </div>
-                        <div className="card-title">
-                            Defaults
-                        </div>
-                        <div className="card-text">
-                            56
-                        </div>
-                    </Card>
+                                <FcList></FcList>
+                            </div>
+                            <div className="card-title">
+                                Loans
+                            </div>
+                            <div className="card-text">
+                                1,356
+                            </div>
+                        </Card>
+                    </div>
+                    <div className="col my-col">
+                        <Card title="returns" height="10rem" width="16rem">
+                        <div className="card-icon">
+                                <FcSalesPerformance></FcSalesPerformance>
+                            </div>
+                            <div className="card-title">
+                                Portfolio return
+                            </div>
+                            <div className="card-text">
+                                14,65%
+                            </div>
+                        </Card>
+                    </div>
+                    {/* <div className="col-md-3 my-col">
+                        <Card title="returns" height="10rem" width="22rem">
+                        <div className="card-icon">
+                                <FcBookmark></FcBookmark>
+                            </div>
+                            <div className="card-title">
+                                Defaults
+                            </div>
+                            <div className="card-text">
+                                56
+                            </div>
+                        </Card>
+                    </div> */}
+                    </div>
+                </Card>
+                <div className="col-2">
+                    <Card></Card>
                 </div>
             </div>
-            <div className="col-md-11 mt-5">
-                <Card title="Line chart" height="30rem" width="95rem">
-                    <Linechart />
-                </Card>
+                <div className="col-md-11 mt-5">
+                    <Card title="Line chart" height="30rem" width="95rem">
+                        {/* <Linechart /> */}
+                    </Card>
+                </div>
                 
             </div>
-        </div>
+        
     )
 }
 
